bbc5d856b35d15212894627d6b0cad0a9b8d4d81
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-06-02 19:23:07 +0530

    lets relase 2

---
+++ Terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "03.58.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+  features {}
+}
---
+++ Terraform/varible.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "can-central"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "sps-cloud-rc-azure"
+}
+
+variable "accout_name" {
+  type = string
+
+}
